bc3e4835fbcda09ebaeb63b68d43dff745b3201c
gamzat echilov <echilov.gamzat@wb.ru>
2025-02-19 09:17:33 +0300

    [b-4.1] Добавить поле additional для customer

--- internal/models/customer.go
+++ internal/models/customer.go
@@ -1,7 +1,8 @@
 package models
 
 type Customer struct {
-	Name    string
-	Address string
-	Phone   string
+	Name       string                 `bson:"name"`
+	Address    string                 `bson:"address"`
+	Phone      string                 `bson:"phone"`
+	Additional map[string]interface{} `bson:"additional_info"`
 }

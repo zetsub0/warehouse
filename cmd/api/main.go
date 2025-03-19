99c1f50bd1d829579973bc4cef5738b4e2dba8f3
gamzat echilov <echilov.gamzat@wb.ru>
2025-03-19 10:12:54 +0300

    [b-8] Веб-сервер

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"context"
+
+	"warehouse/internal/app/http"
+	"warehouse/internal/config"
+)
+
 func main() {
+	ctx := context.Background()
+
+	cfg := config.ParseConfig()
+	srv := http.New(cfg.HTTPServer, nil)
+
+	srv.Run(ctx)
 }
